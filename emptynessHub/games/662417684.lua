6246e92168050be9dbc5cc4912d730e8e1bddb03
Emptyness <89524326+TheEmptynessProject@users.noreply.github.com>
2023-05-19 21:50:58 +0100

    Create 662417684.lua

---
+++ emptynessHub/games/662417684.lua
@@ -0,0 +1,16 @@
+    local default = main:Tab("Main")
+    local test =
+        default:Section(
+        {
+            Name = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name,
+            column = 1
+        }
+    )
+    test:Button(
+        {
+            Name = "Spawn Lucky Block",
+            Callback = function()
+                game:GetService("ReplicatedStorage").SpawnLuckyBlock:FireServer()
+            end
+        }
+    )
