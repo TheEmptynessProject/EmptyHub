4ca883f2eec459ef6d239bda6fc4a48e713b5805
Emptyness <89524326+TheEmptynessProject@users.noreply.github.com>
2023-05-20 17:56:27 +0100

    Update notificationLib.lua

--- emptynessHub/notificationLib.lua
+++ emptynessHub/notificationLib.lua
@@ -1,121 +1,107 @@
 local custom = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheEmptynessProject/EmptynessProject/main/emptynessHub/customFunctions.lua"))()
 
-local pos = {
-    ["Middle"] = UDim2.new(0.445, 0, 0.7, 0),
-    ["MiddleRight"] = UDim2.new(0.85, 0, 0.7, 0),
-    ["MiddleLeft"] = UDim2.new(0.01, 0, 0.7, 0),
-    ["Top"] = UDim2.new(0.445, 0, 0.007, 0),
-    ["TopLeft"] = UDim2.new(0.06, 0, 0.001, 0),
-    ["TopRight"] = UDim2.new(0.8, 0, 0.001, 0)
-}; 
-
--- functions
-
-function fadeObject(object, onTweenCompleted)
-    local tweenInformation = custom.tween(object, {0.3, Enum.EasingStyle.Sine, Enum.EasingDirection.InOut}, {
+local function fadeObject(object, onTweenCompleted)
+    local tweenInfo = custom.animate(object, {0.3, Enum.EasingStyle.Sine, Enum.EasingDirection.InOut}, {
         TextTransparency = 1,
         TextStrokeTransparency = 1
-    }, onTweenCompleted);
+    }, onTweenCompleted)
 end
 
-local notifications = {}; do 
-    function notifications.new(settings)
-        assert(settings, "missing argument #1 in function notifications.new(settings)");
-        assert(typeof(settings) == "table", string.format("expected table for argument #1 in function notifications.new(settings), got %s", typeof(settings)));
-        
-        local notificationSettings = {ui = {notificationsFrame = nil, notificationsFrame_UIListLayout = nil}};
-        
-        for setting, value in next, settings do 
-            notificationSettings[setting] = value 
-        end
-        
-        setmetatable(notificationSettings, {__index = notifications});
-        return notificationSettings
+local notifications = {}
+
+function notifications.new(settings)
+    local notificationSettings = {
+        ui = {
+            frame = nil,
+            layout = nil
+        },
+        lifetime = 3,
+        textColor = Color3.new(1, 1, 1),
+        textSize = 14,
+        textStrokeTransparency = 0,
+        textStrokeColor = Color3.new(0, 0, 0),
+        textFont = Enum.Font.Ubuntu
+    }
+
+    for setting, value in next, settings do
+        notificationSettings[setting] = value
     end
 
-    function notifications:SetNotificationLifetime(lifetime)
-        assert(lifetime, "Missing argument #1 in function SetNotificationLifetime(lifetime)")
-        assert(typeof(lifetime) == "number", string.format("Expected number for argument #1 in function SetNotificationLifetime, got %s", typeof(lifetime)))
-        self.NotificationLifetime = lifetime
+    setmetatable(notificationSettings, {__index = notifications})
+    return notificationSettings
+end
+
+function notifications:SetLifetime(lifetime)
+    self.lifetime = lifetime
+end
+
+function notifications:SetTextColor(color)
+    self.textColor = color
+end
+
+function notifications:SetTextSize(size)
+    self.textSize = size
+end
+
+function notifications:SetTextStrokeTransparency(transparency)
+    self.textStrokeTransparency = transparency
+end
+
+function notifications:SetTextStrokeColor(color)
+    self.textStrokeColor = color
+end
+
+function notifications:SetTextFont(font)
+    self.textFont = typeof(font) == "string" and Enum.Font[font] or font
+end
+
+function notifications:BuildUI()
+    if notifications.screenGui then
+        notifications.screenGui:Destroy()
     end
 
-    function notifications:SetTextColor(color)
-        assert(color, "Missing argument #1 in function SetTextColor(color)")
-        assert(typeof(color) == "Color3", string.format("Expected Color3 for argument #1 in function SetTextColor, got %s", typeof(color)))
-        self.TextColor = color
-    end
+    notifications.screenGui = custom.createObject("ScreenGui", {ZIndexBehavior = Enum.ZIndexBehavior.Sibling, Parent = emptyCustoms})
+    self.ui.frame = custom.create("Frame", {
+        Name = "notificationsFrame",
+        Parent = notifications.screenGui,
+        BackgroundColor3 = Color3.new(1, 1, 1),
+        BackgroundTransparency = 1,
+        Position = UDim2.new(0.8, 0, 0.001, 0),
+        Size = UDim2.new(0, 236, 0, 215)
+    })
 
-    function notifications:SetTextSize(size)
-        assert(size, "Missing argument #1 in function SetTextSize(size)")
-        assert(typeof(size) == "number", string.format("Expected number for argument #1 in function SetTextSize, got %s", typeof(size)))
-        self.TextSize = size
-    end
+    custom.enableDrag(self.ui.frame, 0.1)
 
-    function notifications:SetTextStrokeTransparency(transparency)
-        assert(transparency, "Missing argument #1 in function SetTextStrokeTransparency(transparency)")
-        assert(typeof(transparency) == "number", string.format("Expected number for argument #1 in function SetTextStrokeTransparency, got %s", typeof(transparency)))
-        self.TextStrokeTransparency = transparency
-    end
+    self.ui.layout = custom.create("UIListLayout", {
+        Name = "notificationsFrame_UIListLayout",
+        Parent = self.ui.frame,
+        Padding = UDim.new(0, 1 + self.textSize),
+        SortOrder = Enum.SortOrder.LayoutOrder
+    })
+end
 
-    function notifications:SetTextStrokeColor(color)
-        assert(color, "Missing argument #1 in function SetTextStrokeColor(color)")
-        assert(typeof(color) == "Color3", string.format("Expected Color3 for argument #1 in function SetTextStrokeColor, got %s", typeof(color)))
-        self.TextStrokeColor = color
-    end
+function notifications:Notify(text)
+    assert(type(text) == "string", "Invalid argument #1 in function Notify(text). Expected string.")
+    local notification = custom.createObject("TextLabel", {
+        Name = "notification",
+        Parent = self.ui.frame,
+        BackgroundColor3 = Color3.new(1, 1, 1),
+        BackgroundTransparency = 1,
+        Size = UDim2.new(0, 222, 0, 14),
+        Text = text,
+        Font = self.textFont,
+        TextColor3 = self.textColor,
+        TextSize = self.textSize,
+        TextStrokeColor3 = self.textStrokeColor,
+        TextStrokeTransparency = self.textStrokeTransparency
+    })
+    notification.Active = true
 
-    function notifications:SetTextFont(font)
-        assert(font, "Missing argument #1 in function SetTextFont(font)")
-        assert(typeof(font) == "string" or typeof(font) == "EnumItem", string.format("Expected string or EnumItem for argument #1 in function SetTextFont, got %s", typeof(font)))
-        self.TextFont = Enum.Font[font]
-    end
-    
-    function notifications:BuildNotificationUI()
-        if notifications_screenGui then
-            notifications_screenGui:Destroy()
-        end
-
-        notifications_screenGui = custom.create("ScreenGui", {ZIndexBehavior = Enum.ZIndexBehavior.Sibling, Parent = emptyCustoms})
-        self.ui.notificationsFrame = custom.create("Frame", {
-            Name = "notificationsFrame",
-            Parent = notifications_screenGui,
-            BackgroundColor3 = Color3.fromRGB(255, 255, 255),
-            BackgroundTransparency = 1.000,
-            Position = pos[self.NotificationPosition],
-            Size = UDim2.new(0, 236, 0, 215)
-        })
-
-        custom.drag(self.ui.notificationsFrame, 0.1)
-
-        self.ui.notificationsFrame_UIListLayout = custom.create("UIListLayout", {
-            Name = "notificationsFrame_UIListLayout",
-            Parent = self.ui.notificationsFrame,
-            Padding = UDim.new(0, 1 + self.TextSize),
-            SortOrder = Enum.SortOrder.LayoutOrder
-        })
-    end
-
-    function notifications:Notify(text)
-        local notif = custom.create("TextLabel", {
-            Name = "notification",
-            Parent = self.ui.notificationsFrame,
-            BackgroundColor3 = Color3.fromRGB(255, 255, 255),
-            BackgroundTransparency = 1.000,
-            Size = UDim2.new(0, 222, 0, 14),
-            Text = text,
-            Font = self.TextFont,
-            TextColor3 = self.TextColor,
-            TextSize = self.TextSize,
-            TextStrokeColor3 = self.TextStrokeColor,
-            TextStrokeTransparency = self.TextStrokeTransparency
-        })
-        notif.Active = true
-
-        task.delay(self.NotificationLifetime, function()
-            fadeObject(notif, function()
-                notif:Destroy()
-            end)
+    task.delay(self.lifetime, function()
+        fadeObject(notification, function()
+            notification:Destroy()
         end)
-    end
+    end)
 end
 
-return notifications 
+return notifications
