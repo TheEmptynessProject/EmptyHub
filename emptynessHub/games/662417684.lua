7d5b95bdd1e291f944792fd75b5becc9ca7dbadc
Emptyness <89524326+TheEmptynessProject@users.noreply.github.com>
2023-05-21 00:54:15 +0100

    Update 662417684.lua

--- emptynessHub/games/662417684.lua
+++ emptynessHub/games/662417684.lua
@@ -14,3 +14,37 @@
             end
         }
     )
+test:Button(
+        {
+            Name = "Spawn Super Block",
+            Callback = function()
+                game:GetService("ReplicatedStorage").SpawnSuperBlock:FireServer()
+            end
+        }
+    )
+test:Button(
+        {
+            Name = "Spawn Diamond Block",
+            Callback = function()
+                game:GetService("ReplicatedStorage").SpawnDiamondBlock:FireServer()
+            end
+        }
+    )
+test:Button(
+        {
+            Name = "Spawn Rainbow Block",
+            Callback = function()
+                game:GetService("ReplicatedStorage").SpawnRainbowBlock:FireServer()
+            end
+        }
+    )
+test:Button(
+        {
+            Name = "Spawn Galaxy Block",
+        Tooltip = "test",
+        tooltip = "test2",
+            Callback = function()
+                game:GetService("ReplicatedStorage").SpawnGalaxyBlock:FireServer()
+            end
+        }
+    )
