1e2269a239b298a979e630f4877a9fef0aedd2d4
Emptyness <89524326+TheEmptynessProject@users.noreply.github.com>
2023-05-20 01:04:09 +0100

    Update and rename testPermission.lua to testPermission

--- testPermission.lua
+++
@@ -1,3 +0,0 @@
-[[/TKLfPCEcQAA`ENTvQ#%%_X$J(Y:J<<:6gQ8NZ wKbC@+f7yTN Wc O=$ ~3.aG_Cf\DEdRBDeTB@lUKDtXSV`5";X+i2#{>^{*7,1G`.I("L'WzBrV,F$!YeX fC*H82^8AvIBL`QEC`QCCgRCMaPADdUSItGG$*3:N#&2\/y\;"
-=VG?n9] F]5SUGW0:HBI81A/R
-o)+# O%)/PQX)]]
