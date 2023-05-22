3630d8d46a0369bcd9117acbb49ec5e1a1e31be9
Emptyness <89524326+TheEmptynessProject@users.noreply.github.com>
2023-05-22 20:53:43 +0100

    Update index.lua

--- index.lua
+++ index.lua
@@ -226,6 +226,99 @@ if not (getgenv()[custom.generateString(32, 0)]) then
                     local targetRootPart = targetCharacter and targetCharacter:FindFirstChild("HumanoidRootPart")
                     local playerRootPart = player.Character and player.Character:FindFirstChild("HumanoidRootPart")
                     
+                    if targetRootPart and playerRootPart then
+                        local targetCFrame = targetRootPart.CFrame
+                        local targetLookVector = targetCFrame.LookVector
+                        local offset = targetLookVector * -2
+
+                        player.Character.HumanoidRootPart.CFrame = targetCFrame + offset
+                    end
+                end
+
+                local function checkPlayerReach()
+                    local p = game.Players:GetPlayers()
+                    for i = 2, #p do
+                        local v = p[i]
+                        local vCharacter = v.Character
+                        if
+                            vCharacter and vCharacter:FindFirstChild("Humanoid") and
+                                not vCharacter:FindFirstChildOfClass("ForceField") and
+                                vCharacter.Humanoid.Health > 0 and
+                                vCharacter:FindFirstChild("HumanoidRootPart") and
+                                player:DistanceFromCharacter(vCharacter.HumanoidRootPart.Position) <= range
+                         then
+                            local tool = player.Character and player.Character:FindFirstChildOfClass("Tool")
+                            if tool and tool:FindFirstChild("Handle") then
+                                tool:Activate()
+                                for _, part in ipairs(vCharacter:GetDescendants()) do
+                                    if part:IsA("BasePart") then
+                                        firetouchinterest(tool.Handle, part, 0)
+                                        firetouchinterest(tool.Handle, part, 1)
+                                    end
+                                end
+                            end
+                            teleportToBehindPlayer(v)
+                        end
+                    end
+                end
+
+                mouse.KeyDown:Connect(handleKeyDown)
+                game.Players.PlayerAdded:Connect(updateAdornment)
+                game.Players.PlayerRemoving:Connect(updateAdornment)
+                game:GetService("RunService").RenderStepped:Connect(checkPlayerReach)
+                updateAdornment()
+            end
+        }
+    )
+    universalColumn1:CreateButton(
+        {
+            Name = "Tp Tool Reach Team Check",
+            Callback = function()
+                local range = 15
+                local player = game:GetService("Players").LocalPlayer
+                local mouse = player:GetMouse()
+
+                local function adorn(part, radius)
+                    local sphereAdornment = Instance.new("SphereHandleAdornment")
+                    sphereAdornment.Name = "Reach"
+                    sphereAdornment.Adornee = part
+                    sphereAdornment.Radius = radius
+                    sphereAdornment.Transparency = 0.8
+                    sphereAdornment.Color3 = Color3.new(1, 1, 1)
+                    sphereAdornment.AlwaysOnTop = true
+                    sphereAdornment.ZIndex = -1
+                    sphereAdornment.Parent = part
+                end
+
+                local function updateAdornment()
+                    local head = player.Character and player.Character:FindFirstChild("HumanoidRootPart")
+                    if head then
+                        local reachAdornment = head:FindFirstChild("Reach")
+                        if reachAdornment and reachAdornment:IsA("SphereHandleAdornment") then
+                            reachAdornment.Radius = range
+                        else
+                            adorn(head, range)
+                        end
+                    end
+                end
+
+                local function handleKeyDown(key)
+                    if key == "c" then
+                        range = range + 2
+                        notifLib:Notify("Range Increased")
+                        updateAdornment()
+                    elseif key == "v" then
+                        range = range - 2
+                        notifLib:Notify("Range Decreased")
+                        updateAdornment()
+                    end
+                end
+
+                local function teleportToBehindPlayer(targetPlayer)
+                    local targetCharacter = targetPlayer.Character
+                    local targetRootPart = targetCharacter and targetCharacter:FindFirstChild("HumanoidRootPart")
+                    local playerRootPart = player.Character and player.Character:FindFirstChild("HumanoidRootPart")
+                    
                     if targetRootPart and playerRootPart and game.Players.LocalPlayer.Team ~= targetPlayer.Team then
                         local targetCFrame = targetRootPart.CFrame
                         local targetLookVector = targetCFrame.LookVector
