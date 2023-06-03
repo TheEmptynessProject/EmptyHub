082df4323f0071c727fb408d9982e82c9417cb16
Emptyness <89524326+TheEmptynessProject@users.noreply.github.com>
2023-06-03 16:47:42 +0100

    Update 537413528.lua

--- emptynessHub/games/537413528.lua
+++ emptynessHub/games/537413528.lua
@@ -1,24 +1,36 @@
 local default = getgenv().mainLib:NewTab("Game Tab 1")
-    local PlaceId =
-        default:NewSection(
-        {
-            Name = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name,
-            column = 1
-        }
-    )
-    PlaceId:CreateButton(
-        {
-            Name = "Disable Water Damage",
-            Callback = function()
-            for i,v in pairs(game.workspace:GetDescendants()) do
+local PlaceId =
+    default:NewSection(
+    {
+        Name = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name,
+        column = 1
+    }
+)
+PlaceId:CreateButton(
+    {
+        Name = "Disable Water Damage",
+        Callback = function()
+            for i, v in pairs(game.workspace:GetDescendants()) do
                 if v.Name == "Water" then
-                local wah_uh = v:Clone()
+                    local wah_uh = v:Clone()
                     local wah_uh_parent = v.Parent
                     wah_uh.Parent = nil
                     v:Destroy()
                     wah_uh.Parent = wah_uh_parent
-                    end
                 end
             end
-        }
-    )
+        end
+    }
+)
+PlaceId:CreateButton(
+    {
+        Name = "Fire All Thrusters",
+        Callback = function()
+            for i, v in pairs(game.Workspace:GetDescendants()) do
+                if string.find(v.Parent.Name, "Thruster") and v:IsA("ClickDetector") then
+                    fireclickdetector(v, 0)
+                end
+            end
+        end
+    }
+)
