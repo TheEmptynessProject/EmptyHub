1b2a1615c129789048ce1945407ac6b4e319af80
Emptyness <89524326+TheEmptynessProject@users.noreply.github.com>
2023-06-06 00:21:36 +0100

    Update 537413528.lua

--- emptynessHub/games/537413528.lua
+++ emptynessHub/games/537413528.lua
@@ -190,3 +190,115 @@ PlaceId:CreateToggle(
         end
     }
 )
+universalColumn1:CreateLine(2, Color3.new(255, 0, 255))
+local dropdownItemArray = {}
+local protecEnabled = false
+for _, item in pairs(game.ReplicatedStorage.BuildingParts:GetChildren()) do
+if (item:IsA("Model")) then
+table.insert(dropdownItemArray, item.Name)
+end
+end
+universalColumn1:CreateLabel("Item to use")
+PlaceId:CreateDropdown(
+        {
+            Content = dropdownPlayerArray,
+            MultiChoice = false,
+            Callback = function(selection)
+                    
+            end
+        }
+    )
+PlaceId:CreateToggle(
+        {
+            Name = "Protection",
+            Callback = function(bool)
+                protecEnabled = bool
+            end
+        }
+    )
+PlaceId:CreateButton(
+    {
+        Name = "Fire",
+        Callback = function()
+            local RunService = game:GetService("RunService")
+            local Players = game:GetService("Players")
+            local LocalPlayer = Players.LocalPlayer
+            local Character = LocalPlayer.Character
+            local CoreGui = game:GetService("CoreGui")
+            local RobloxGui = CoreGui.RobloxGui
+            local Workspace = game:GetService("Workspace")
+            if protecEnabled then
+            local overlay = Instance.new("Frame", RobloxGui)
+            overlay.BackgroundColor3 = Color3.new(0, 0, 0)
+            overlay.Size = UDim2.fromScale(1, 1)
+
+            local function set3DRenderingEnabled(enabled)
+                RunService:Set3dRenderingEnabled(enabled)
+            end
+end
+            local teamString = tostring(LocalPlayer.TeamColor) .. "Zone"
+
+            local TNTargs = {
+                    [1] = "SticksOfTNT",
+                    [2] = 180,
+                    [3] = Workspace[teamString],
+                    [4] = CFrame.new(10, 7.6, -124) * CFrame.Angles(-math.pi, 0, -math.pi),
+                    [5] = true,
+                    [6] = 1,
+                    [7] = CFrame.new(-43.565689086914, -13.399991989136, -464.50686645508) *
+                        CFrame.Angles(-math.pi, 0, -math.pi),
+                    [8] = false
+                }
+
+            local function invokeServerBatch(toolArgs)
+                Character.BuildingTool.RF:InvokeServer(unpack(toolArgs))
+            end
+if protecEnabled then
+            local function temp(enabled)
+                overlay.Visible = enabled
+                set3DRenderingEnabled(not enabled)
+            end
+
+            temp(true)
+                end
+
+			game.workspace.PVPRemote:FireServer(true)
+local seatArgs = {
+                    [1] = "Seat",
+                    [2] = 1065,
+                    [3] = Workspace[teamString],
+                    [4] = CFrame.new(10, 6.6, -120) * CFrame.Angles(-math.pi, 0, -math.pi),
+                    [5] = false,
+                    [6] = 1,
+                    [7] = CFrame.new(-43.565689086914, -12.399991989136, -465.50686645508) *
+                        CFrame.Angles(-math.pi, 0, -math.pi),
+                    [8] = false
+                }
+            for i = 1, 20 do
+            local temp = seatArgs[4]
+            	seatArgs[4] = seatArgs[4]+Vector3.new(0,i/50,0)
+                invokeServerBatch(seatArgs)
+                task.wait()
+                seatArgs[4] = temp
+            end
+            for i = 1, 1 do
+            local temp = TNTargs[4]
+            	TNTargs[4] = TNTargs[4]+Vector3.new(0,1,0)
+                invokeServerBatch(TNTargs)
+                task.wait()
+            TNTargs[4] = temp
+            end
+            
+            for _, v in ipairs(Workspace:GetChildren()) do
+                if string.find(v.Name, "TNT") and v:FindFirstChild("PPart") then
+                    v.PPart.ActivateRemote:FireServer()
+                end
+            end
+task.wait(0.5)
+            if protecEnabled then
+            temp(false)
+                end
+            game.workspace.PVPRemote:FireServer(false)
+        end
+    }
+)
