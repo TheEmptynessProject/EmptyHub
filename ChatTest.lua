eb5203657f7065839cf1af5e5d2f253f458e65ce
Emptyness <89524326+TheEmptynessProject@users.noreply.github.com>
2023-06-05 19:27:55 +0100

    Update ChatTest.lua

--- ChatTest.lua
+++ ChatTest.lua
@@ -1,2 +1,2 @@
-local returnArray = {{"username":"test","content":"Hey","msgId":0},{"username":"testUsern","content":"How are you?","msgId":1},{"username":"newUser","content":"This is a new message.","msgId":2}}
+local returnArray = {{"username"="test","content"="Hey","msgId"=0},{"username"="testUsern","content"="How are you?","msgId"=1},{"username"="newUser","content"="This is a new message.","msgId"=2}}
 return returnArray
