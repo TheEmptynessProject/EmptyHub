18e5c0c6f0d70ff58da4a14d9fae9cf186d84f82
DummyTestAcc-token <135647769+DummyTestAcc-token@users.noreply.github.com>
2023-06-05 19:46:47 +0100

    Chatted at 1685990804.748767

--- ChatTest.lua
+++ ChatTest.lua
@@ -1,1 +1,1 @@
-[{"username":"test","content":"Hey","msgId":0},{"username":"testUsern","content":"How are you?","msgId":1},{"username":"newUser","content":"This is a new message.","msgId":2}]
+[{"username":"test","content":"Hey","msgId":0},{"username":"testUsern","content":"How are you?","msgId":1},{"username":"newUser","content":"This is a new message.","msgId":2},{"username":"HakerPT","content":"hey","msgId":3}]
