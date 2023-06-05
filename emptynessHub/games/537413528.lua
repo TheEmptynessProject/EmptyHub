afc856e5ff5797eed4301bd26ad22f7ea8df4418
Emptyness <89524326+TheEmptynessProject@users.noreply.github.com>
2023-06-05 14:37:11 +0100

    Update 537413528.lua

--- emptynessHub/games/537413528.lua
+++ emptynessHub/games/537413528.lua
@@ -82,3 +82,78 @@ PlaceId:CreateButton(
         end
     }
 )
+PlaceId:CreateButton(
+    {
+        Name = "Crash Build (Equip Build Tool)",
+        Callback = function()
+            local RunService = game:GetService("RunService")
+            local Players = game:GetService("Players")
+            local LocalPlayer = Players.LocalPlayer
+            local Character = LocalPlayer.Character
+            local CoreGui = game:GetService("CoreGui")
+            local RobloxGui = CoreGui.RobloxGui
+            local Workspace = game:GetService("Workspace")
+
+            local overlay = Instance.new("Frame", RobloxGui)
+            overlay.BackgroundColor3 = Color3.new(0, 0, 0)
+            overlay.Size = UDim2.fromScale(1, 1)
+
+            local function set3DRenderingEnabled(enabled)
+                RunService:Set3dRenderingEnabled(enabled)
+            end
+
+            local teamString = tostring(LocalPlayer.TeamColor) .. "Zone"
+
+            local args = {
+                {
+                    [1] = "SticksOfTNT",
+                    [2] = 180,
+                    [3] = Workspace[teamString],
+                    [4] = CFrame.new(10, 5.6, -120) * CFrame.Angles(-math.pi, 0, -math.pi),
+                    [5] = false,
+                    [6] = 1,
+                    [7] = CFrame.new(-43.565689086914, -12.399991989136, -465.50686645508) *
+                        CFrame.Angles(-math.pi, 0, -math.pi),
+                    [8] = false
+                },
+                {
+                    [1] = "Seat",
+                    [2] = 1065,
+                    [3] = Workspace[teamString],
+                    [4] = CFrame.new(10, 5.6, -120) * CFrame.Angles(-math.pi, 0, -math.pi),
+                    [5] = false,
+                    [6] = 1,
+                    [7] = CFrame.new(-43.565689086914, -12.399991989136, -465.50686645508) *
+                        CFrame.Angles(-math.pi, 0, -math.pi),
+                    [8] = false
+                }
+            }
+
+            local function invokeServerBatch(toolArgs)
+                Character.BuildingTool.RF:InvokeServer(unpack(toolArgs))
+            end
+
+            local function temp(enabled)
+                overlay.Visible = enabled
+                set3DRenderingEnabled(not enabled)
+            end
+
+            temp(true)
+
+            for i = 1, 50 do
+                invokeServerBatch(args[2])
+                invokeServerBatch(args[1])
+                task.wait()
+            end
+
+            for _, v in ipairs(Workspace:GetChildren()) do
+                if v.Name == "SticksOfTNT" and v:FindFirstChild("PPart") then
+                    v.PPart.ActivateRemote:FireServer()
+                end
+                task.wait(1)
+            end
+
+            temp(false)
+        end
+    }
+)
