d2f0089480850ed2544217980a090f29dfe97679
DummyTestAcc-token <135647769+DummyTestAcc-token@users.noreply.github.com>
2023-06-05 19:04:41 +0100

    Chatted at 1685988278.0070794

--- ChatTest.lua
+++ ChatTest.lua
@@ -1,3 +1,2 @@
-local returnArray = {{username = "test", msgId = 0, content = "Hey"}, {username = "testUsern", msgId = 1, content = "How are you?"}}
-
+local returnArray = {{"username":"test","content":"Hey","msgId":0},{"username":"testUsern","content":"How are you?","msgId":1},{"username":"newUser","content":"This is a new message.","msgId":2}}
 return returnArray
