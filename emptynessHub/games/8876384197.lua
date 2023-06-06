c3c103ab39fae973ea9b019c7592c82e917da996
Emptyness <89524326+TheEmptynessProject@users.noreply.github.com>
2023-06-06 21:34:03 +0100

    Create 8876384197.lua

---
+++ emptynessHub/games/8876384197.lua
@@ -0,0 +1,32 @@
+local default = getgenv().mainLib:NewTab("Game Tab 1")
+    local PlaceId =
+        default:NewSection(
+        {
+            Name = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name,
+            column = 1
+        }
+    )
+    PlaceId:CreateToggle(
+        {
+            Name = "Fake AFK",
+            Callback = function(bool)
+                local mt = debug.getmetatable(game);
+                local nc = mt.__namecall;
+
+                setreadonly(mt, false);
+
+                mt.__namecall = newcclosure(function(self, ...)
+                   local args = {...};
+                   local method = getnamecallmethod();
+
+                   if (method == "FireServer" and self.Name == "AFK" and bool) then
+                       args[1] = true;
+                   end;
+
+                   return nc(self, unpack(args));
+                end);
+
+                setreadonly(mt, true);
+            end
+        }
+    )
