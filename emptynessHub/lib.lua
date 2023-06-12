8d190f256397ea57f24850aee5776bd3718fb4a8
Emptyness <89524326+TheEmptynessProject@users.noreply.github.com>
2023-06-12 21:19:00 +0100

    Update lib.lua

--- emptynessHub/lib.lua
+++ emptynessHub/lib.lua
@@ -1385,7 +1385,6 @@ function library:New(opts)
                     }
                 )
                 
-
                 custom.createObject(
                     "UICorner",
                     {
