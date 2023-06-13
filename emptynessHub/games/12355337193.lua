818cbfa65e23f85d3589d42ae3b6c1bdcb94048f
Emptyness <89524326+TheEmptynessProject@users.noreply.github.com>
2023-06-13 14:11:44 +0100

    Update 12355337193.lua

--- emptynessHub/games/12355337193.lua
+++ emptynessHub/games/12355337193.lua
@@ -1,30 +1,107 @@
 local default = getgenv().mainLib:NewTab("Game Tab 1")
-    local PlaceId =
-        default:NewSection(
-        {
-            Name = "",
-            column = 1
-        }
-    )
-    PlaceId:CreateKeybind(
-        {
-            Name = "KillAll",
-            Default = Enum.KeyCode.R,
-            Callback = function()
-                for i,v in pairs(game.Players:GetChildren()) do
-                    game:GetService("ReplicatedStorage").Remotes.Stab:FireServer(v.Character.HumanoidRootPart)
-                end
-                --OR
-                -- for i,v in pairs(game.Players:GetChildren()) do
-                -- local args = {
-                    -- [1] = Vector3.new(0,0,0),
-                    -- [2] = Vector3.new(0,0,0),
-                    -- [3] = v.Character.HumanoidRootPart,
-                    -- [4] = Vector3.new(0,0,0)
-                -- }
-
-                -- game:GetService("ReplicatedStorage").Remotes.Shoot:FireServer(unpack(args))
-                -- end
+local PlaceId =
+    default:NewSection(
+    {
+        Name = "",
+        column = 1
+    }
+)
+local function getToolEquipped()
+    for i, v in pairs(game.workspace:GetChildren()) do
+        if v.Name == game.Players.LocalPlayer.Name then
+            local tool = v:FindFirstChildOfClass("Tool")
+            if not tool then
+                return nil
             end
-        }
-    )
+            if tool:FindFirstChild("Fire") and tool.Fire:IsA("Sound") then
+                return true --pistol
+            else
+                return false --knife
+            end
+        end
+    end
+    return nil
+end
+PlaceId:CreateKeybind(
+    {
+        Name = "KillAll",
+        Default = Enum.KeyCode.R,
+        Callback = function()
+            local pistol = getToolEquipped()
+            repeat
+                pistol = getToolEquipped()
+                task.wait(0.2)
+            until pistol ~= nil
+            if pistol then
+                for i, v in pairs(game.workspace:GetChildren()) do
+                    if
+                        v.ClassName == "Model" and v.Name ~= "" and v:FindFirstChild("HumanoidRootPart") and
+                            v.Name ~= game.Players.LocalPlayer.Name
+                     then
+                        local args = {
+                            [1] = Vector3.new(0, 0, 0),
+                            [2] = Vector3.new(0, 0, 0),
+                            [3] = v.HumanoidRootPart,
+                            [4] = Vector3.new(0, 0, 0)
+                        }
+
+                        game:GetService("ReplicatedStorage").Remotes.Shoot:FireServer(unpack(args))
+                    end
+                end
+            else
+                for i, v in pairs(game.workspace:GetChildren()) do
+                    if
+                        v.ClassName == "Model" and v.Name ~= "" and v:FindFirstChild("HumanoidRootPart") and
+                            v.Name ~= game.Players.LocalPlayer.Name
+                     then
+                        game:GetService("ReplicatedStorage").Remotes.Stab:FireServer(v.HumanoidRootPart)
+                    end
+                end
+            end
+        end
+    }
+)
+
+PlaceId:CreateKeybind(
+    {
+        Name = "Kill one",
+        Default = Enum.KeyCode.G,
+        Callback = function()
+            local pistol = getToolEquipped()
+            repeat
+                pistol = getToolEquipped()
+                task.wait(0.2)
+            until pistol ~= nil
+            if pistol then
+                for i, v in pairs(game.workspace:GetChildren()) do
+                    if
+                        v.ClassName == "Model" and v.Name ~= "" and v:FindFirstChild("HumanoidRootPart") and
+                            v.Name ~= game.Players.LocalPlayer.Name
+                     then
+                        if game.Players[v].Team ~= nil or game.Players[v].Team ~= game.Players.LocalPlayer.Team then
+                            local args = {
+                                [1] = Vector3.new(0, 0, 0),
+                                [2] = Vector3.new(0, 0, 0),
+                                [3] = v.HumanoidRootPart,
+                                [4] = Vector3.new(0, 0, 0)
+                            }
+
+                            game:GetService("ReplicatedStorage").Remotes.Shoot:FireServer(unpack(args))
+                        end
+                    end
+                end
+            else
+                for i, v in pairs(game.workspace:GetChildren()) do
+                    if
+                        v.ClassName == "Model" and v.Name ~= "" and v:FindFirstChild("HumanoidRootPart") and
+                            v.Name ~= game.Players.LocalPlayer.Name
+                     then
+                        if game.Players[v].Team ~= nil or game.Players[v].Team ~= game.Players.LocalPlayer.Team then
+                            game:GetService("ReplicatedStorage").Remotes.Stab:FireServer(v.HumanoidRootPart)
+                        end
+                    end
+                end
+            end
+        end
+    }
+)
