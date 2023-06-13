f135e16e3eeb0c17e73b418f1672395eccf68bde
Emptyness <89524326+TheEmptynessProject@users.noreply.github.com>
2023-06-13 01:18:49 +0100

    Update 9961650006.lua

--- emptynessHub/games/9961650006.lua
+++ emptynessHub/games/9961650006.lua
@@ -20,6 +20,17 @@ PlaceId:CreateToggle(
         Name = "Autofarm",
         Callback = function(bool)
             while bool do
+                local max = false
+                if not max then
+                for i, v in ipairs(game.Workspace.Coins.Points:GetChildren()) do
+                                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
+                                i=i+1
+                                task.wait(0.1)
+                if i >= 10 then 
+                           max = true
+                        end
+                            end  
+                    end
             game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.workspace.Finish.Finish.CFrame
             task.wait(1)
                 end
