c39efd7dfcfa606d5264f10bf3c3597d1c692449
Emptyness <89524326+TheEmptynessProject@users.noreply.github.com>
2023-06-13 04:44:43 +0100

    Update 12046823603.lua

--- emptynessHub/games/12046823603.lua
+++ emptynessHub/games/12046823603.lua
@@ -11,13 +11,13 @@ PlaceId:CreateToggle(
         Name = "Get All Blocks",
         Callback = function(toggled)
             while true do
-                if not toggled then break end
+                if not toggled then return end
                 for i, v in ipairs(game.workspace.Spawn_Part:GetDescendants()) do
                     if v:FindFirstChild("TouchInterest") then
                         firetouchinterest(v, game.Players.LocalPlayer.Character.HumanoidRootPart, 0)
                         firetouchinterest(v, game.Players.LocalPlayer.Character.HumanoidRootPart, 1)
                     end
-                    if not toggled then break end
+                    if not toggled then return end
                 end
                 
                 task.wait()
