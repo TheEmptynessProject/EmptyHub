d78b9043140c2091dd333a64ef9fa2ef9813babd
Emptyness <89524326+TheEmptynessProject@users.noreply.github.com>
2023-06-13 01:14:40 +0100

    Update 9961650006.lua

--- emptynessHub/games/9961650006.lua
+++ emptynessHub/games/9961650006.lua
@@ -17,13 +17,28 @@ PlaceId:CreateKeybind(
         end
     }
 )
+PlaceId:CreateToggle(
+    {
+        Name = "Autofarm",
+        Callback = function(bool)
+            while bool do
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.workspace.Finish.Finish.CFrame
+            task.wait(0.5)
+            game.Players.LocalPlayer.Character.Humanoid:ChangeState("Jumping")
+                end
+        end
+    }
+)
 PlaceId:CreateButton(
     {
         Name = "Grab Coins",
         Callback = function()
+            local max = 10
             for i, v in ipairs(game.Workspace.Coins.Points:GetChildren()) do
                                 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
+                                i=i+1
                                 task.wait(0.1)
+                if i >= 10 then return end
                             end           
         end
     }
