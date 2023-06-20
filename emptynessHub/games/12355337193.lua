0df45965190ca12edab64f1c7b0aa38d369e8697
Emptyness <89524326+TheEmptynessProject@users.noreply.github.com>
2023-06-20 16:36:58 +0100

    Update 12355337193.lua

--- emptynessHub/games/12355337193.lua
+++ emptynessHub/games/12355337193.lua
@@ -38,7 +38,7 @@ PlaceId:CreateKeybind(
                         v.ClassName == "Model" and v.Name ~= "" and v:FindFirstChild("HumanoidRootPart") and
                             v.Name ~= game.Players.LocalPlayer.Name
                      then
-                        if game.Players[v.Name].Team ~= nil or game.Players[v.Name].Team ~= game.Players.LocalPlayer.Team and v.Humanoid.Health > 0 then
+                        if game.Players[v.Name].Team ~= nil and game.Players[v.Name].Team ~= game.Players.LocalPlayer.Team and v.Humanoid.Health > 0 then
                             local args = {
                                 [1] = Vector3.new(0, 0, 0),
                                 [2] = Vector3.new(0, 0, 0),
@@ -56,7 +56,7 @@ PlaceId:CreateKeybind(
                         v.ClassName == "Model" and v.Name ~= "" and v:FindFirstChild("HumanoidRootPart") and
                             v.Name ~= game.Players.LocalPlayer.Name
                      then
-                        if game.Players[v.Name].Team ~= nil or game.Players[v.Name].Team ~= game.Players.LocalPlayer.Team and v.Humanoid.Health > 0 then
+                        if game.Players[v.Name].Team ~= nil and game.Players[v.Name].Team ~= game.Players.LocalPlayer.Team and v.Humanoid.Health > 0 then
                             game:GetService("ReplicatedStorage").Remotes.Stab:FireServer(v.HumanoidRootPart)
                         end
                     end
@@ -83,7 +83,6 @@ PlaceId:CreateKeybind(
                             v.Name ~= game.Players.LocalPlayer.Name
                      then
                         if game.Players[v.Name].Team ~= nil and game.Players[v.Name].Team ~= game.Players.LocalPlayer.Team and v.Humanoid.Health > 0 then
-                            print(v.Name)
                             local args = {
                                 [1] = Vector3.new(0, 0, 0),
                                 [2] = Vector3.new(0, 0, 0),
@@ -103,12 +102,88 @@ PlaceId:CreateKeybind(
                             v.Name ~= game.Players.LocalPlayer.Name
                      then
                         if game.Players[v.Name].Team ~= nil and game.Players[v.Name].Team ~= game.Players.LocalPlayer.Team and v.Humanoid.Health > 0 then
-                            print(v.Name)
                             
                             game:GetService("ReplicatedStorage").Remotes.Stab:FireServer(v.HumanoidRootPart)
                             return
                         end
                     end
+                end
+            end
+        end
+    }
+)
+local delay = 0
+universalColumn2:CreateSlider(
+        {
+            Name = "WalkSpeed",
+            Min = 0,
+            Max = 5,
+            Default = 0,
+            Decimals = 1,
+            Callback = function(value)
+               delay = value
+            end
+        }
+    )
+PlaceId:CreateToggle(
+    {
+        Name = "Silent Aim",
+        Default = Enum.KeyCode.R,
+        Callback = function()
+            local mouse = game.Players.LocalPlayer:GetMouse()
+            local pistol = getToolEquipped()
+
+            repeat
+                pistol = getToolEquipped()
+                task.wait(0.2)
+            until pistol ~= nil
+
+            if pistol then
+                local nearestPlayer, nearestDistance = nil, math.huge
+
+                for _, player in ipairs(game.Players:GetPlayers()) do
+                    if player ~= game.Players.LocalPlayer and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+                        local playerRootPart = player.Character.HumanoidRootPart
+                        local distance = (playerRootPart.Position - mouse.Hit.p).Magnitude
+
+                        if distance < nearestDistance then
+                            nearestPlayer = player
+                            nearestDistance = distance
+                        end
+                    end
+                end
+
+                if nearestPlayer and nearestPlayer.Team and nearestPlayer.Team ~= game.Players.LocalPlayer.Team then
+
+                    local args = {
+                        [1] = Vector3.new(0, 0, 0),
+                        [2] = Vector3.new(0, 0, 0),
+                        [3] = nearestPlayer.Character.HumanoidRootPart,
+                        [4] = Vector3.new(0, 0, 0)
+                    }
+
+                    task.wait(delay)
+                    game:GetService("ReplicatedStorage").Remotes.Shoot:FireServer(unpack(args))
+                end
+            else
+                local nearestPlayer, nearestDistance = nil, math.huge
+
+                for _, player in ipairs(game.Players:GetPlayers()) do
+                    if player ~= game.Players.LocalPlayer and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+                        local playerRootPart = player.Character.HumanoidRootPart
+                        local distance = (playerRootPart.Position - mouse.Hit.p).Magnitude
+
+                        if distance < nearestDistance then
+                            nearestPlayer = player
+                            nearestDistance = distance
+                        end
+                    end
+                end
+
+                if nearestPlayer and nearestPlayer.Team and nearestPlayer.Team ~= game.Players.LocalPlayer.Team then
+
+                    task.wait(delay)
+                    game:GetService("ReplicatedStorage").Remotes.Stab:FireServer(nearestPlayer.Character.HumanoidRootPart)
                 end
             end
         end
