a2f38e1a6e07d86eda474005b2f15dfb4fea313e
Emptyness <89524326+TheEmptynessProject@users.noreply.github.com>
2023-06-20 20:40:28 +0100

    Update 12355337193.lua

--- emptynessHub/games/12355337193.lua
+++ emptynessHub/games/12355337193.lua
@@ -130,62 +130,85 @@ PlaceId:CreateToggle(
         Name = "Silent Aim",
         Default = Enum.KeyCode.R,
         Callback = function()
+            local function getToolEquipped()
+    for i, v in pairs(game.workspace:GetChildren()) do
+        if v.Name == game.Players.LocalPlayer.Name then
+            local tool = v:FindFirstChildOfClass("Tool")
+            if not tool then
+                return nil
+            end
+            if tool:FindFirstChild("Fire") and tool.Fire:IsA("Sound") then
+                return true --pistol
+            else
+                return false --knife
+            end
+        end
+    end
+    return nil
+end
+
+local function test(mouseHit)
+    local nearestPlayer, nearestDistance = nil, math.huge
+
+    for _, player in ipairs(game.Players:GetPlayers()) do
+        if
+            player ~= game.Players.LocalPlayer and player.Character and
+                player.Character:FindFirstChild("HumanoidRootPart")
+         then
+            local playerRootPart = player.Character.HumanoidRootPart
+            local distance = (playerRootPart.Position - mouseHit).Magnitude
+
+            if
+                distance < nearestDistance and player.Team and player.Team ~= game.Players.LocalPlayer.Team and
+                    player.Character.Humanoid and
+                    player.Character.Humanoid.Health > 0
+             then
+                nearestPlayer = player
+                nearestDistance = distance
+            end
+        end
+    end
+    if nearestPlayer then
+        return nearestPlayer
+    end
+end
+local UserInputService = game:GetService("UserInputService")
+
+local function onMouseButton1Click(mouse)
+    local mouseLocation = mouse.X, mouse.Y
+    local worldPosition = mouse.Hit.Position
+    return worldPosition
+end
+
+UserInputService.InputBegan:Connect(
+    function(input, isProcessed)
+        if input.UserInputType == Enum.UserInputType.MouseButton1 then
             local mouse = game.Players.LocalPlayer:GetMouse()
+            local nearestToMouse = test(onMouseButton1Click(mouse))
             local pistol = getToolEquipped()
 
             repeat
                 pistol = getToolEquipped()
                 task.wait(0.2)
             until pistol ~= nil
-
             if pistol then
-                local nearestPlayer, nearestDistance = nil, math.huge
+                local args = {
+                    [1] = Vector3.new(0, 0, 0),
+                    [2] = Vector3.new(0, 0, 0),
+                    [3] = nearestToMouse.Character.HumanoidRootPart,
+                    [4] = Vector3.new(0, 0, 0)
+                }
 
-                for _, player in ipairs(game.Players:GetPlayers()) do
-                    if player ~= game.Players.LocalPlayer and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
-                        local playerRootPart = player.Character.HumanoidRootPart
-                        local distance = (playerRootPart.Position - mouse.Hit.p).Magnitude
-
-                        if distance < nearestDistance then
-                            nearestPlayer = player
-                            nearestDistance = distance
-                        end
-                    end
-                end
-
-                if nearestPlayer and nearestPlayer.Team and nearestPlayer.Team ~= game.Players.LocalPlayer.Team then
-
-                    local args = {
-                        [1] = Vector3.new(0, 0, 0),
-                        [2] = Vector3.new(0, 0, 0),
-                        [3] = nearestPlayer.Character.HumanoidRootPart,
-                        [4] = Vector3.new(0, 0, 0)
-                    }
-
-                    task.wait(delay)
-                    game:GetService("ReplicatedStorage").Remotes.Shoot:FireServer(unpack(args))
-                end
+                task.wait(delay)
+                game:GetService("ReplicatedStorage").Remotes.Shoot:FireServer(unpack(args))
             else
-                local nearestPlayer, nearestDistance = nil, math.huge
-
-                for _, player in ipairs(game.Players:GetPlayers()) do
-                    if player ~= game.Players.LocalPlayer and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
-                        local playerRootPart = player.Character.HumanoidRootPart
-                        local distance = (playerRootPart.Position - mouse.Hit.p).Magnitude
-
-                        if distance < nearestDistance then
-                            nearestPlayer = player
-                            nearestDistance = distance
-                        end
-                    end
-                end
-
-                if nearestPlayer and nearestPlayer.Team and nearestPlayer.Team ~= game.Players.LocalPlayer.Team then
-
-                    task.wait(delay)
-                    game:GetService("ReplicatedStorage").Remotes.Stab:FireServer(nearestPlayer.Character.HumanoidRootPart)
-                end
+                task.wait(delay)
+                game:GetService("ReplicatedStorage").Remotes.Stab:FireServer(nearestToMouse.Character.HumanoidRootPart)
             end
+        end
+    end
+)
+
         end
     }
 )
