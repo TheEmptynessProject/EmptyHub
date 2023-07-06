d8ea781aa04fd26f9025e93849f96add99707e60
Emptyness <89524326+TheEmptynessProject@users.noreply.github.com>
2023-07-06 05:26:00 +0100

    Update 3214114884.lua

--- emptynessHub/games/3214114884.lua
+++ emptynessHub/games/3214114884.lua
@@ -1,17 +1,16 @@
 local default = getgenv().mainLib:NewTab("Game Tab 1")
-    local PlaceId =
-        default:NewSection(
-        {
-            Name = "",
-            column = 1
-        }
-    )
+local PlaceId =
+    default:NewSection(
+    {
+        Name = "",
+        column = 1
+    }
+)
+local lastGetFlagExecutionTime = 0
 PlaceId:CreateButton(
     {
         Name = "Get Flag",
         Callback = function()
-            local lastGetFlagExecutionTime = 0
-
             local currentTime = tick()
             local timeDifference = currentTime - lastGetFlagExecutionTime
             local remainingSeconds = math.floor(30 - timeDifference)
@@ -36,8 +35,40 @@ PlaceId:CreateButton(
                     end
                 end
             else
-                notifLib:Notify(("Please wait for " .. remainingSeconds .. " seconds before executing the script again."), {Color = Color3.new(255, 255, 255)})
+                notifLib:Notify(
+                    ("Please wait for " .. remainingSeconds .. " seconds before executing the script again."),
+                    {Color = Color3.new(255, 255, 255)}
+                )
                 task.wait(1)
+            end
+        end
+    }
+)
+PlaceId:CreateKeybind(
+    {
+        Name = "Damage All",
+        Default = Enum.KeyCode.J,
+        Callback = function()
+            for i, v in pairs(game.Players:GetPlayers()) do
+                if v ~= game.Players.LocalPlayer then
+                    local args = {
+                        [1] = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Tool"),
+                        [2] = {
+                            ["p"] = v.Character.HumanoidRootPart.Position,
+                            ["pid"] = 1,
+                            ["part"] = v.Character.HumanoidRootPart,
+                            ["d"] = 1,
+                            ["maxDist"] = 1,
+                            ["h"] = v.Character.HumanoidRootPart,
+                            ["m"] = Enum.Material.Plastic,
+                            ["sid"] = 0,
+                            ["t"] = 0,
+                            ["n"] = Vector3.new(0, 0, 0)
+                        }
+                    }
+
+                    game:GetService("ReplicatedStorage").WeaponsSystem.Network.WeaponHit:FireServer(unpack(args))
+                end
             end
         end
     }
