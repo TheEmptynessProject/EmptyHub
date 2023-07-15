4c9d7692eb2d44280d3901d0242ee035ae730de4
Emptyness <89524326+TheEmptynessProject@users.noreply.github.com>
2023-07-15 13:07:45 +0100

    Update 3351674303.lua

--- emptynessHub/games/3351674303.lua
+++ emptynessHub/games/3351674303.lua
@@ -7,9 +7,11 @@ local PlaceId =
     }
 )
 local function getVehicle()
-local temp = game.workspace.Vehicles:FindFirstChild(tostring(game.Players.LocalPlayer.Name))
+    local temp = game.workspace.Vehicles:FindFirstChild(tostring(game.Players.LocalPlayer.Name))
     if temp then
-		return temp
+        return temp
+	else
+notifLib:Notify("Error: Car not found!", {Color = Color3.new(255, 0, 0)})
 	end
     return nil
 end
@@ -18,9 +20,9 @@ PlaceId:CreateSlider(
     {
         Name = "Boost Multiplier",
         Min = 0,
-        Max = 8500000,
+        Max = 50000,
         Default = 0,
-        Decimals = 0.0001,
+        Decimals = 0.001,
         Callback = function(value)
             multi = value
         end
@@ -31,12 +33,229 @@ PlaceId:CreateKeybind(
         Name = "Boost",
         Default = Enum.KeyCode.E,
         Callback = function()
-            local temp = getVehicle()
-            if not temp or not temp:FindFirstChild("Weight") then
-                notifLib:Notify("Error: Car not found!", {Color = Color3.new(255, 0, 0)})
-                return
+            getVehicle().Weight._boost.force = workspace.CurrentCamera.CFrame.LookVector * multi
+        end
+    }
+)
+local function apply(front, both)
+    for i, v in pairs(getVehicle().Constraints:GetChildren()) do
+        if both then
+            if string.find(v.Name, "Spring") then
+                v.Damping = Damping
+                v.FreeLength = Length
+                v.LimitsEnabled = limitsEnabled
+                v.Visible = Visible
+                v.Radius = Radius
+                v.MaxForce = MaxForce
+                v.Coils = Coils
+                v.Stiffness = Stiffness
+                v.Enabled = Enabled
+                v.Thickness = Thickness
             end
-            temp.Weight._boost.force = workspace.CurrentCamera.CFrame.LookVector * multi
+        else
+            if front then
+                if string.find(v.Name, "FrontSpring") then
+                    v.Damping = Damping
+                    v.FreeLength = Length
+                    v.LimitsEnabled = limitsEnabled
+                    v.Visible = Visible
+                    v.Radius = Radius
+                    v.MaxForce = MaxForce
+                    v.Coils = Coils
+                    v.Stiffness = Stiffness
+                    v.Enabled = Enabled
+                    v.Thickness = Thickness
+                end
+            else
+                if string.find(v.Name, "RearSpring") then
+                    v.Damping = Damping
+                    v.FreeLength = Length
+                    v.LimitsEnabled = limitsEnabled
+                    v.Visible = Visible
+                    v.Radius = Radius
+                    v.MaxForce = MaxForce
+                    v.Coils = Coils
+                    v.Stiffness = Stiffness
+                    v.Enabled = Enabled
+                    v.Thickness = Thickness
+                end
+            end
+        end
+    end
+end
+local Damping, Length, limitsEnabled, Visible, Radius, MaxForce, Coils, Stiffness, Enabled, Thickness = 0
+local affectFront, bothAffected = nil
+local autoApply = false
+universalColumn1:CreateDropdown(
+    {
+        Content = {"Front", "Rear"},
+        MultiChoice = true,
+        Callback = function(selection)
+            if #selection == 2 then
+                bothAffected = true
+            else
+                bothAffected = false
+                if selection == "Front" then
+                    affectFront = true
+                elseif selection == "Rear" then
+                    affectFront = false
+                end
+            end
+        end
+    }
+)
+PlaceId:CreateSlider(
+    {
+        Name = "Damping",
+        Min = 0,
+        Max = 10000,
+        Default = 400,
+        Decimals = 0.01,
+        Callback = function(value)
+            Damping = value
+            if autoApply then
+                apply(affectFront, bothAffected)
+            end
+        end
+    }
+)
+PlaceId:CreateSlider(
+    {
+        Name = "Length",
+        Min = 0,
+        Max = 10,
+        Default = 2,
+        Decimals = 0.0001,
+        Callback = function(value)
+            Length = value
+            if autoApply then
+                apply(affectFront, bothAffected)
+            end
+        end
+    }
+)
+PlaceId:CreateSlider(
+    {
+        Name = "Radius",
+        Info = "Visual Only",
+        Min = 0,
+        Max = 10,
+        Default = 1,
+        Decimals = 1,
+        Callback = function(value)
+            Radius = value
+            if autoApply then
+                apply(affectFront, bothAffected)
+            end
+        end
+    }
+)
+PlaceId:CreateSlider(
+    {
+        Name = "Max Force",
+        Min = 0,
+        Max = 10000000,
+        Default = 1000000,
+        Decimals = 0.0001,
+        Callback = function(value)
+            MaxForce = value
+            if autoApply then
+                apply(affectFront, bothAffected)
+            end
+        end
+    }
+)
+PlaceId:CreateSlider(
+    {
+        Name = "Coils",
+        Min = 0,
+        Max = 8,
+        Default = 2,
+        Decimals = 0.1,
+        Callback = function(value)
+            Coils = value
+            if autoApply then
+                apply(affectFront, bothAffected)
+            end
+        end
+    }
+)
+PlaceId:CreateSlider(
+    {
+        Name = "Stiffness",
+        Min = 0,
+        Max = 50000,
+        Default = 22000,
+        Decimals = 0.01,
+        Callback = function(value)
+            Stiffness = value
+            if autoApply then
+                apply(affectFront, bothAffected)
+            end
+        end
+    }
+)
+PlaceId:CreateSlider(
+    {
+        Name = "Thickness",
+        Min = 0,
+        Max = 1000,
+        Default = 0.1,
+        Decimals = 0.1,
+        Callback = function(value)
+            Thickness = value
+            if autoApply then
+                apply(affectFront, bothAffected)
+            end
+        end
+    }
+)
+PlaceId:CreateToggle(
+    {
+        Name = "Limits Enabled",
+        Callback = function(bool)
+            limitsEnabled = bool
+            if autoApply then
+                apply(affectFront, bothAffected)
+            end
+        end
+    }
+)
+PlaceId:CreateToggle(
+    {
+        Name = "Visible",
+        Callback = function(bool)
+            Visible = bool
+            if autoApply then
+                apply(affectFront, bothAffected)
+            end
+        end
+    }
+)
+PlaceId:CreateToggle(
+    {
+        Name = "Enabled",
+        Callback = function(bool)
+            Enabled = bool
+            if autoApply then
+                apply(affectFront, bothAffected)
+            end
+        end
+    }
+)
+PlaceId:CreateToggle(
+    {
+        Name = "Auto Apply",
+        Callback = function(bool)
+            autoApply = bool
+        end
+    }
+)
+PlaceId:CreateButton(
+    {
+        Name = "Apply",
+        Callback = function()
+            apply(affectFront, bothAffected)
         end
     }
 )
