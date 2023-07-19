e475e0a729a9bf4d3158cc7048a5d81c3ab5b010
Emptyness <89524326+TheEmptynessProject@users.noreply.github.com>
2023-07-19 19:28:49 +0100

    Update 1962086868.lua

--- emptynessHub/games/1962086868.lua
+++ emptynessHub/games/1962086868.lua
@@ -26,27 +26,3 @@ PlaceId:CreateButton(
         end
     }
 )
-local thing
-PlaceId:CreateToggle(
-    {
-        Name = "Infinite Jump Bypass",
-        Callback = function(toggled)
-        if not thing then
-        thing = Instance.new("Part")
-        thing.Anchored = true
-        thing.Parent = game.Workspace
-        thing.Size = Vector3.new(5, 0.1, 5)
-		thing.Transparency = 1
-        end
-	while true do
-        thing.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame - Vector3.new(0,3.3,0)
-        if not toggled then
-          thing:Destroy()
-          thing = nil
-          break
-          end
-        task.wait()
-        end
-        end
-    }
-)
