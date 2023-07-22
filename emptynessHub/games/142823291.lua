1334273445143a3b94475dd3845abc7e4451be5c
Emptyness <89524326+TheEmptynessProject@users.noreply.github.com>
2023-07-22 14:24:42 +0100

    Update 142823291.lua

--- emptynessHub/games/142823291.lua
+++ emptynessHub/games/142823291.lua
@@ -24,15 +24,14 @@ local function getClosestCoin()
             end
         end
     end
-    if closestCoin then
-        return closestCoin
-    end
+    return closestCoin
 end
 PlaceId:CreateToggle(
     {
         Name = "Coin Farm - Path TP",
         Callback = function(bool)
-            while bool do
+            while task.wait() do
+                if not bool then return end
                 local coin = getClosestCoin()
                 if coin then
                     local path = PathfindingService:CreatePath()
@@ -51,7 +50,8 @@ PlaceId:CreateToggle(
     {
         Name = "Coin Farm - Path Walk",
         Callback = function(bool)
-            while bool do
+            while task.wait() do
+                if not bool then return end
                 local coin = getClosestCoin()
                 if coin then
                     local path = PathfindingService:CreatePath()
@@ -72,7 +72,8 @@ PlaceId:CreateToggle(
     {
         Name = "Coin Farm - CFrame TP",
         Callback = function(bool)
-            while bool do
+            while task.wait() do
+                if not bool then return end
                 local coin = getClosestCoin()
                 if coin then
                     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = coin.CFrame
@@ -86,7 +87,8 @@ PlaceId:CreateToggle(
     {
         Name = "Coin Farm - Tween Distance",
         Callback = function(bool)
-            while bool do
+            while task.wait() do
+                if not bool then return end
                 local coin = getClosestCoin()
                 if coin then
                     local tweenTime = closestDistance / 75
@@ -111,7 +113,8 @@ PlaceId:CreateToggle(
     {
         Name = "Coin Farm - Tween Time",
         Callback = function(bool)
-            while bool do
+            while task.wait() do
+                if not bool then return end
                 local coin = getClosestCoin()
                 if coin then
                     local tweenInfo = TweenInfo.new(5, Enum.EasingStyle.Quint, Enum.EasingDirection.Out)
@@ -133,21 +136,7 @@ PlaceId:CreateToggle(
 PlaceId:CreateButton(
     {
         Name = "Teleport to Safe Place",
-        Callback = function(bool)
-            while bool do
-                local coin = getClosestCoin()
-                if coin then
-                    local path = PathfindingService:CreatePath()
-                    path:ComputeAsync(character.HumanoidRootPart.Position, coin.Position)
-
-                    local waypoints = path:GetWaypoints()
-                    for _, waypoint in ipairs(waypoints) do
-                        character.Humanoid:MoveTo(waypoint.Position)
-                        character.Humanoid.MoveToFinished:Wait()
-                        wait(0.1)
-                    end
-                end
-            end
+        Callback = function()
         end
     }
 )
@@ -163,13 +152,15 @@ PlaceId:CreateToggle(
     {
         Name = "No Radios",
         Callback = function(bool)
-            while bool do
-                for i, v in pairs(game.workspace:GetDescendants()) do
-                    if v.Parent ~= "Lobby" and v.Name == "Radio" then
+            while task.wait() do
+                if not bool then return end
+                for i, v in pairs(game.Players:GetPlayers()) do
+                    if v ~= player then
+                    if v.Character and v:FindFirstChild("Radio") then
                         v:Destroy()
                     end
+                    end
                 end
-                task.wait()
             end
         end
     }
