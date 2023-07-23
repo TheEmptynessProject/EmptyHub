c8bd68797d0a4bbee507f316c67ae73913d8adda
Emptyness <89524326+TheEmptynessProject@users.noreply.github.com>
2023-07-23 21:50:13 +0100

    Update 654732683.lua

--- emptynessHub/games/654732683.lua
+++ emptynessHub/games/654732683.lua
@@ -35,7 +35,7 @@ PlaceId:CreateSlider(
     {
         Name = "Boost Multiplier",
         Min = 0,
-        Max = 5000,
+        Max = 100,
         Callback = function(a)
             Bmulti = a
         end
